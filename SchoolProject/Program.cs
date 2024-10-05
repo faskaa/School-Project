820e01f9afa0f2a4be6df067fe0e8e01db18d26b
faskaa <fazilnb@code.edu.az>
2024-10-05 21:30:11 +0400

    Admin area created

--- SchoolProject/Program.cs
+++ SchoolProject/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "Areas",
+        pattern: "{area:exists}/{controller=DashBoard}/{action=Index}"
+        );
+});
+//app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/", () => "Hello World!");
-
+app.UseStaticFiles();
 app.Run();

596c7632f724c7ab37c0f9e218796870dacdbfa1
faskaa <fazilnb@code.edu.az>
2024-10-09 01:01:16 +0400

    News logic complated , Bugs fixed in UI

--- SchoolProject/Areas/Admin/Controllers/NewsController.cs
+++ SchoolProject/Areas/Admin/Controllers/NewsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SchoolProject.DAL;
 using SchoolProject.Entities;
+using System.Xml;
 
 namespace SchoolProject.Areas.Admin.Controllers
 {
@@ -19,6 +20,67 @@ namespace SchoolProject.Areas.Admin.Controllers
             List<Xeberler> News = _context.Xeberler.ToList();
             //return Json(News);
             return View(News);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Xeberler xeber)
+        {
+            if (xeber == null) return BadRequest();
+            if (string.IsNullOrEmpty(xeber.Name) || string.IsNullOrEmpty(xeber.Description))
+            {
+                ModelState.AddModelError("Null", "Please Fill Form");
+                return View();
+
+            }
+            _context.Xeberler.Add(xeber);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            Xeberler xeber = _context.Xeberler.FirstOrDefault(x => x.Id == id)!;
+            if (xeber == null) return BadRequest();
+            return View(xeber);
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, Xeberler edited)
+        {
+            if (edited == null) return BadRequest();
+            if (string.IsNullOrEmpty(edited.Name) || string.IsNullOrEmpty(edited.Description))
+            {
+                ModelState.AddModelError("Null", "Please Fill Form");
+                return View();
+            }
+
+            Xeberler xeber = _context.Xeberler.FirstOrDefault(x => x.Id == id)!;
+            if (xeber == null) return BadRequest();
+            xeber.Name = edited.Name;
+            xeber.Description = edited.Description;
+            _context.SaveChanges();
+            //Xeberler xeber = _context.Xeberler.FirstOrDefault(x => x.Id == id)!;
+            //if (xeber == null) return BadRequest();
+            return RedirectToAction("Index");
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Xeberler xeber = _context.Xeberler.FirstOrDefault(_x => _x.Id == id)!;
+            if(xeber == null) return BadRequest();
+            _context.Xeberler.Remove(xeber);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
